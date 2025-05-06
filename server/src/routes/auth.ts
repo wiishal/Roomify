7bb8a694e3a1fff36ace0136e088f4800d173270
wiishal <vishalnanaware7850@gmail.com>
2025-05-07 00:32:19 +0530

    modify: modified routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import {
   CheckCreadential,
+  LoginInUser,
   SignInUser,
   signToken,
 } from "../service/service.auth";
-
-const users = [{ username: "v", password: "1" }];
 
 const AuthRouter = express.Router();
 
@@ -13,19 +12,22 @@ const AuthRouter = express.Router();
 
 AuthRouter.post("/signin", async (req, res) => {
   const { userdetails } = req.body;
-
+  console.log("sign : ", userdetails);
   const isCredentialExisted = await CheckCreadential(userdetails);
 
   if (!isCredentialExisted.success) {
     res.status(404).json({ message: isCredentialExisted.message });
     return;
   }
-  if (isCredentialExisted.isEmailExisted) {
-    res.status(404).json({ message: "email is already exist" });
+  if (isCredentialExisted.isUserExist && isCredentialExisted.isEmailExisted) {
+    res.status(401).json({ message: "email is already exist" });
     return;
   }
-  if (!isCredentialExisted.isUsernameExisted) {
-    res.status(404).json({ message: "username is already exist" });
+  if (
+    isCredentialExisted.isUserExist &&
+    isCredentialExisted.isUsernameExisted
+  ) {
+    res.status(401).json({ message: "username is already exist" });
     return;
   }
 
@@ -40,7 +42,33 @@ AuthRouter.post("/signin", async (req, res) => {
 
 //login
 AuthRouter.post("/login", async (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ message: "Invalide request" });
+    return;
+  }
   const { userdetails } = req.body;
+  //zod
+  console.log("login : ", userdetails);
+  const loguser = await LoginInUser(userdetails);
+  if (!loguser.success) {
+    res.status(401).json({ message: loguser.message });
+    return;
+  }
+  if (!loguser.user) {
+    res.status(501).json({ message: "Internal server error" });
+    return;
+  }
+  const currentToken = await signToken({
+    username: loguser.user.username,
+    id: loguser.user.id,
+  });
+  if (!currentToken) {
+    res.status(404).json({ messgae: "failed during generating token" });
+    return;
+  }
+  console.log("token generated : ", currentToken);
+  res.status(200).json({ token: currentToken, message: "user logged" });
+  return;
 });
 
 AuthRouter.post("/verify", (req, res) => {
--- server/src/routes/server.ts
+++ server/src/routes/server.ts
@@ -1,4 +1,10 @@
 import express from "express";
+import { tokenVerification } from "../middleware/tokenVerification";
+import {
+  CreateDefaultChannels,
+  CreateServer,
+  GetServers,
+} from "../service/service.server";
 const server = [
   {
     roomid: "1",
@@ -22,11 +28,21 @@ const server = [
 ];
 
 const ServerRouter = express.Router();
+ServerRouter.use(tokenVerification);
 
-ServerRouter.get("/", (req, res) => {
-  const s = server.map((s) => ({ roomid: s.roomid, name: s.name }));
-  console.log(s);
-  res.json({ servers: s ,message:"servers fetched successfully"});
+ServerRouter.get("/", async (req, res) => {
+  const user = req.user;
+  const servers = await GetServers();
+  console.log(servers);
+  if (!servers.success) {
+    res.status(404).json({ message: servers.message });
+    return;
+  }
+  res.json({
+    servers: servers.servers,
+    message: "servers fetched successfully",
+    user: user,
+  });
 });
 
 ServerRouter.get("/serverinfo/:id", (req, res) => {
@@ -44,9 +60,38 @@ ServerRouter.get("/serverinfo/:id", (req, res) => {
   });
 });
 
-ServerRouter.post("/create", (req, res) => {
+ServerRouter.post("/create", async (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ message: "Invalide request" });
+    return;
+  }
   const { server } = req.body;
-  console.log(server);
-  res.json({ server: server, message: "server is created" });
+  const user = req.user;
+
+  if (!user) {
+    res.status(500).json({ message: "failed during auth decode" });
+    console.log("error : user decode missing ");
+    return;
+  }
+  console.log(user);
+  const createdServer = await CreateServer({
+    serverName: server.name,
+    userid: user.id,
+    serverDes: server.description,
+  });
+
+  if (!createdServer.success) {
+    res.status(401).json({ message: createdServer.message });
+    return;
+  }
+  if (!createdServer.server) {
+    return;
+  }
+  const createchannels = await CreateDefaultChannels(createdServer.server?.id);
+  if (!createchannels.success) {
+    res.status(403).json({ message: createchannels.message });
+    return;
+  }
+  res.status(200).json({ message: "server Created Successfully" });
 });
 export default ServerRouter;
