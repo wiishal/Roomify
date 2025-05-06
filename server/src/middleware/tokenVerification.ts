e5e0c84b709a275fe89bd268abe061ea9a8ad33c
wiishal <vishalnanaware7850@gmail.com>
2025-05-07 00:33:48 +0530

    Add: added middleware for token verification

---
+++ server/src/middleware/tokenVerification.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+const secret = process.env.JWT_SECRET;
+
+export function tokenVerification(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ message: "unauthorized" });
+    return;
+  }
+  try {
+    if (!secret) {
+      throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+    const decoded = jwt.verify(token, secret) as jwt.JwtPayload;
+    console.log("decoded token", decoded);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: "internal server error" });
+    console.log("middleware error: error while verfying token :", error);
+  }
+}
