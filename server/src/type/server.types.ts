bea34211862e3f2ea1924433c52d541f493445d4
wiishal <vishalnanaware7850@gmail.com>
2025-05-11 21:13:13 +0530

    modified: server types

--- server/src/type/server.types.ts
+++ server/src/type/server.types.ts
@@ -5,6 +5,12 @@ export interface createServerInputs {
   userid: number;
   serverDes: string;
 }
-export interface CreateServer extends BaseResponce {
+export interface CreateServerResponse extends BaseResponse {
   server?: Server;
+}
+export interface GetServersResponse extends BaseResponse {
+  servers?: Server[];
+}
+export interface GetServerInfoResponse extends BaseResponse {
+  server?: Server | null;
 }
