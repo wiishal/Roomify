f43bec761fa2601e13021aeab5d104a8b6738d11
wiishal <vishalnanaware7850@gmail.com>
2025-06-03 22:21:28 +0530

    Update: sepeated the types

---
+++ client/src/type/Server.type.ts
@@ -0,0 +1,25 @@
+import { Channel, Server } from "./type";
+
+export interface BaseResponse {
+  success: boolean;
+  message?: string;
+  status?: number;
+}
+
+export interface loginResponse extends BaseResponse {
+  token?: string;
+}
+
+export interface FetchChannelResponce extends BaseResponse {
+  channels?: Record<string, Channel[]>;
+  serverInfo?: Server;
+}
+export interface GetServersResponse extends BaseResponse {
+  servers?: Server[];
+}
+export interface CreateServerResponse extends BaseResponse {
+  server?: Server;
+}
+export interface CreateChannelResponse extends BaseResponse {
+  channel?: Channel;
+}
