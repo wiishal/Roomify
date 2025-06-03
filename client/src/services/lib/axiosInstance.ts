54932aad10f811b5b7c6644a0931d2af019903b4
wiishal <vishalnanaware7850@gmail.com>
2025-06-03 22:20:20 +0530

    Add: added interceptor

---
+++ client/src/services/lib/axiosInstance.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const isAuthRequest =
+    config.url?.includes("/api/signup") || config.url?.includes("api/login");
+
+  if (!isAuthRequest) {
+    let token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
+});
+
+export default axiosInstance;
