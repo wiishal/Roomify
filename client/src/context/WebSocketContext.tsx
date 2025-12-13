b5de98fc20c556292ae9f92d2043662a45f8251e
wiishal <vishalnanaware7850@gmail.com>
2025-12-13 20:38:35 +0530

    modify: ws context for reconnect

--- client/src/context/WebSocketContext.tsx
+++ client/src/context/WebSocketContext.tsx
@@ -11,23 +11,30 @@ import { MsgType } from "../type/type";
 
 export const WebSocketContext = createContext<WebSocket | null>(null);
 
+const MAXTRIES = 5;
+const DELAY = 1000;
+
 export const WebSocketProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const { isLogged } = useAuth();
   const [socket, setSocket] = useState<WebSocket | null>(null);
   const socketRef = useRef<WebSocket | null>(null);
+  const retriesCountRef = useRef<number>(0);
+  const retryDelayRef = useRef<number | null>(null);
   const { addMessage } = useMessages();
 
-  useEffect(() => {
+  const connect = () => {
     if (!isLogged) return;
-
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      return;
+    }
     const ws = new WebSocket(import.meta.env.VITE_WS_URL);
     socketRef.current = ws;
     setSocket(ws);
-
     ws.onopen = () => {
       console.log("WebSocket connected");
+      retriesCountRef.current = 0;
       ws.send(
         JSON.stringify({
           type: "auth",
@@ -39,26 +46,39 @@ export const WebSocketProvider: React.FC<{ children: ReactNode }> = ({
     ws.onmessage = (event) => {
       const msg = JSON.parse(event.data);
       if (msg.type === MsgType.roomMessage) {
-        console.log(msg)
+        console.log(msg);
         addMessage(msg.serverId, msg.channelId, msg);
-        console.log('message added : ',msg.serverId)
+        console.log("message added : ", msg.serverId);
       }
-
-      // if (msg.type === "directMessage") {
-      //   addDirectMessage(msg.senderId, msg.message);
-      // }
-
-      // any other message types
     };
 
-    ws.onerror = (err) => console.error("WS error:", err);
-    ws.onclose = () =>{
-      
-    }
-
-    return () => {
+    ws.onerror = (err) => {
+      console.error("WS error:", err);
       ws.close();
-      console.log("WebSocket closed");
+    };
+    ws.onclose = () => {
+      console.log("reconnecting...");
+      if (!isLogged) return;
+
+      if (retriesCountRef.current >= MAXTRIES ) {
+        console.log(`max tries reached!`);
+        return;
+      }
+      const delay = DELAY * Math.pow(2, retriesCountRef.current);
+      retriesCountRef.current++;
+      retryDelayRef.current = window.setTimeout(connect, delay);
+    };
+  };
+  
+  useEffect(() => {
+    connect();
+    return () => {
+      if (retryDelayRef.current != null) {
+        clearTimeout(retryDelayRef.current);
+      }
+      socketRef.current?.close();
+      socketRef.current = null;
+      retriesCountRef.current = 0;
     };
   }, [isLogged]);
 
